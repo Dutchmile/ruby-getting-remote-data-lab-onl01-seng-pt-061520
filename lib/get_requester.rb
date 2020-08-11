2abd3b5441082b909adda81387cc9e8745bfbafe
Christopher Dutcher <cadutcher@icloud.com>
2020-08-11 15:05:33 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+  end
+
+  def parse_json
+  end
+
+end
+
+# https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
